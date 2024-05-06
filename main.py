9554b83126df8f76e9a46b5d6f1631e703aaadb1
wanderer-yfdby <gbkfnd@gmail.com>
2024-05-06 12:14:53 +0500

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGTH =600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+
+pygame.display.set_caption("Игруля Тир")
+icon = pygame.image.load("")
+
 running = True
 while running:
     pass
